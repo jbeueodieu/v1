0ed225ce126fedacddff415743fd73f5074fe9b2
Ahmed Qa <112282987+QaAhmed@users.noreply.github.com>
2022-09-07 22:23:30 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -64,6 +64,9 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Only  Links formats are  accepted for this Var value.
+SUDO = getenv(
+    "SUDO", ""
+)
 CHANNEL_SUDO = getenv(
     "CHANNEL_SUDO", ""
 )
